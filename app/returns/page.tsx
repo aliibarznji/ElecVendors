482980ab221c79c85e4a4f4f939536137dfc190e
Ali <ali.baiz.622@gmail.com>
2026-05-03 23:52:41 +0300

    Add returns page with content, filters, and pagination

---
+++ app/returns/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardShell } from "../dashboard-shell";
+import { ReturnsContent } from "../returns-content";
+
+export default function ReturnsPage() {
+  return (
+    <DashboardShell>
+      <ReturnsContent />
+    </DashboardShell>
+  );
+}
