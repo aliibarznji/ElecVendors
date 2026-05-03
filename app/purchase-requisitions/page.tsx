c6212ae771f79cbee2c75778e086319e57e08b73
Ali <ali.baiz.622@gmail.com>
2026-05-03 23:51:47 +0300

    Add purchase requisitions page and content with filters and table layout

---
+++ app/purchase-requisitions/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardShell } from "../dashboard-shell";
+import { PurchaseRequisitionsContent } from "../purchase-requisitions-content";
+
+export default function PurchaseRequisitionsPage() {
+  return (
+    <DashboardShell>
+      <PurchaseRequisitionsContent />
+    </DashboardShell>
+  );
+}
