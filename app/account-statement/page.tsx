427b65c39128a19e49dd92a1efb3f20dd99d6847
Ali <ali.baiz.622@gmail.com>
2026-05-04 00:00:02 +0300

    Add account statement page with content, filters, and pagination

---
+++ app/account-statement/page.tsx
@@ -0,0 +1,10 @@
+import { AccountStatementContent } from "../account-statement-content";
+import { DashboardShell } from "../dashboard-shell";
+
+export default function AccountStatementPage() {
+  return (
+    <DashboardShell>
+      <AccountStatementContent />
+    </DashboardShell>
+  );
+}
