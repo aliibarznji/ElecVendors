058a05c11240d7a7efe88e4447b0f9c207c8baee
Ali <ali.baiz.622@gmail.com>
2026-05-03 23:57:08 +0300

    Add payments page with content, filters, and pagination

---
+++ app/payments/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardShell } from "../dashboard-shell";
+import { PaymentsContent } from "../payments-content";
+
+export default function PaymentsPage() {
+  return (
+    <DashboardShell>
+      <PaymentsContent />
+    </DashboardShell>
+  );
+}
