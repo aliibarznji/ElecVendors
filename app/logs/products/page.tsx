c4a8dee989fe85790901854f894749c9279ec20e
Ali <ali.baiz.622@gmail.com>
2026-05-04 11:25:43 +0300

    Add Product Logs page and content components with styling enhancements

---
+++ app/logs/products/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardShell } from "../../dashboard-shell";
+import { ProductLogsContent } from "../../product-logs-content";
+
+export default function ProductLogsPage() {
+  return (
+    <DashboardShell>
+      <ProductLogsContent />
+    </DashboardShell>
+  );
+}
