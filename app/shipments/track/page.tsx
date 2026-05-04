8bb888d9e97e6bdc595a5484f7a2c71ad00a0bff
Ali <ali.baiz.622@gmail.com>
2026-05-04 11:11:05 +0300

    add Shipments section

---
+++ app/shipments/track/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardShell } from "../../dashboard-shell";
+import { TrackShipmentsContent } from "../../track-shipments-content";
+
+export default function TrackShipmentsPage() {
+  return (
+    <DashboardShell>
+      <TrackShipmentsContent />
+    </DashboardShell>
+  );
+}
